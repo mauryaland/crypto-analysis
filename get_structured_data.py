f71263f48c80c70d9c9b466819dc2ac42014364a
mauryaland <amaury@fouret.org>
2018-05-26 19:34:29 +0200

    cmc api to dataframe without scheduler

---
+++ get_structured_data.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+# Import modules
+import pandas as pd
+import ijson
+import urllib
+from decimal import Decimal
+
+def api_to_pandas():
+    """
+    This function is used to call the top 100 cryptos ranking from CMC API and to store it into a pandas dataframe
+    """
+    
+    # Get the data
+    f = urllib.request.urlopen('https://api.coinmarketcap.com/v2/ticker/?structure=array')
+    objects = ijson.items(f, 'data.item')
+    columns = list(objects)
+    
+    # Keep only the key attributes of interest
+    main_column_names = [col for col in columns[0].keys() if col != 'quotes']
+    sub_column_names = [col for col in columns[0]['quotes']['USD'].keys()]
+    column_names = main_column_names + sub_column_names
+
+    # Take values from those attributes
+    data = []
+    for rank in range(len(columns)):
+        main_info = []
+        for col in main_column_names:
+            main_info.append(columns[rank][col])
+        # Collect informations economic informations in USD     
+        sub_info = list(columns[rank]['quotes']['USD'].values())      
+        info = main_info + sub_info
+        for index, value in enumerate(info):
+            if type(value) == decimal.Decimal:
+                info[index] = float(value)            
+        data.append(info)
+        
+        # Create a pandas dataframe
+    cmc_df = pd.DataFrame(data, columns=column_names)
+    return cmc_df

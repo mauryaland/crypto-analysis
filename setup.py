84aaa956d61c054d757d01c113ac45b229a00a50
mauryaland <amaury@fouret.org>
2018-06-10 18:40:58 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
+import cryptorocket
 
 setup(name='cryptorocket',
       version=cryptorocket.__version__,
       description='Daily and weekly analysis of the top 100 cryptocurrencies from coinmarketcap',
       url='https://github.com/mauryaland/crypto-analysis',
-	  license='MIT License',
+	license='MIT License',
       author='Amaury Fouret',
-	  author_email='amaury@fouret.org',
+	author_email='amaury@fouret.org',
       packages=['cryptorocket'],
-	  install_requires=[
-	  'pandas>=0.23.0'],
+	install_requires=[
+	  'pandas>=0.23.0',
+        'numpy>=1.14.3',
+        'urllib3>=1.22',
+        'ijson>=2.3',
+        'msgpack>=0.5.6'],
       zip_safe=False)
